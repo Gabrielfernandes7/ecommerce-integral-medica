6f7a8a377f731d7722bbb38b5a9ad43b98ccc150
Gabrielfernandes7 <gabriel.assuncao.fernandes07@aluno.ifce.edu.br>
2022-02-16 16:53:22 -0300

    adicionado dropdown categorias

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 document.getElementById("demo").innerHTML = "Musculação";
+
+/* Adicionado ao carrinho */
+var chartAdd = [];
+
+/* Dropdown */
+/*
+$(function () {
+    $('.dropdown-toggle').dropdown();
+});
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+*/
